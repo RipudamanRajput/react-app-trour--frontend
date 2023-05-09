cdc74d81433d118e4ae3d5bb49d5a8675eab6708
ripudaman-cedcoss <ripudamanrajput@cedcommerce.com>
2023-05-09 12:22:09 +0530

    home page setup 3

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
     return (
       
     <Routes>
-      <Route path='/react-app-trour' element={<Registration />} />
+      <Route path='/react-app-trour--frontend' element={<Registration />} />
       <Route path='/react-app-trour/Login' element={<Login />} />
       <Route
         path='/react-app-trour/panel/*'
